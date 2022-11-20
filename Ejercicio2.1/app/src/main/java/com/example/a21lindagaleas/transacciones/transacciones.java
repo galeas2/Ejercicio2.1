2bd57a8960faf79fbafc1d2f13e0e17ab37c1deb
galeas2 <jaredgaleas@gmail.com>
2022-11-20 17:38:52 -0600

    Initial commit

---
+++ Ejercicio2.1/app/src/main/java/com/example/a21lindagaleas/transacciones/transacciones.java
@@ -0,0 +1,10 @@
+package com.example.a21lindagaleas.transacciones;
+
+public class transacciones {
+    public static final String NameDataBase = "DBActual";
+    public static final String tablavideo = "video";
+    public static final String video = "video";
+    public static final String CreateTableVideo = "CREATE TABLE video(video BLOB)";
+    public static final String DropTableVideo = "DROP TABLE IF EXISTS video";
+
+}
